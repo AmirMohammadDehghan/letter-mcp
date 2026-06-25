a810c422c6aac66993bb55f89969613b0f5eb2d8
AmirMohammadDehghan <ad9191386@gmail.com>
2026-06-25 17:34:15 +0330

    add jazzmin nova

--- core/settings.py
+++ core/settings.py
@@ -60,6 +60,7 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # ---------------------------------------------------------------------
 
 INSTALLED_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
